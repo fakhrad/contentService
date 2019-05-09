ab0d7b799e6042ee927039060ae7ad1e8e11f408
fakhrad <fakhrad@openratio.com>
2019-05-09 11:13:07 +0430

    rabbit link updated

--- src/rpcserver.js
+++ src/rpcserver.js
@@ -5,8 +5,8 @@ const ctypeController = require('./controllers/contentTypeController');
 const assetController = require('./controllers/assetController');
 const categoriesController = require('./controllers/categoryController');
 
-//var rabbitHost = process.env.RABBITMQ_HOST || "amqp://gvgeetrh:6SyWQAxDCpcdg1S0Dc-Up0sUxfmBUVZU@chimpanzee.rmq.cloudamqp.com/gvgeetrh";
-var rabbitHost = process.env.RABBITMQ_HOST || "amqp://localhost:5672";
+var rabbitHost = process.env.RABBITMQ_HOST || "amqp://gvgeetrh:6SyWQAxDCpcdg1S0Dc-Up0sUxfmBUVZU@chimpanzee.rmq.cloudamqp.com/gvgeetrh";
+//var rabbitHost = process.env.RABBITMQ_HOST || "amqp://localhost:5672";
 
 var amqpConn = null;
 function start() {
