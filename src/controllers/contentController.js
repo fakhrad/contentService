3f656c795063c7a3d2fc241c683c0b28a3e9cb41
fakhrad <fakhrad@openratio.com>
2019-05-17 11:00:01 +0430

    update content will update its contentType if it will be null/undefined

--- src/controllers/contentController.js
+++ src/controllers/contentController.js
@@ -266,6 +266,8 @@ var updateContent = function(req, cb)
                 content.statusLog.push(newStatus);
             }
             content.sys.lastUpdater = req.userId;
+            if (content.sys.contentType === undefined || content.sys.contentType === null)
+                content.sys.contentType = req.body.contentType;
             content.sys.lastUpdateTime = new Date();
             content.save(function(err){
                 if(err)
