372d8519fda17a4054e0303ac84ee4fa62d133a4
fakhrad <fakhrad@openratio.com>
2019-05-19 19:05:46 +0430

    removed extra fields from content

--- src/controllers/contentController.js
+++ src/controllers/contentController.js
@@ -25,7 +25,7 @@ var filter = function(req, cb)
     if (!req.body.status)
         delete flt.status;
     console.log(flt);
-    Contents.find(flt).populate('contentType', "title").populate('category', 'name').exec(function(err, contents){
+    Contents.find(flt).populate('contentType', "title").populate('category', 'name').select("name status sys category contentType").exec(function(err, contents){
         var result = {success : false, data : null, error : null };
         if (err)
         {
@@ -54,7 +54,7 @@ var filter = function(req, cb)
 
 var loadContents = function(req, cb)
 {
-    Contents.find({"sys.spaceId" : req.spaceId}).populate('contentType', "title").populate('category', 'name')
+    Contents.find({"sys.spaceId" : req.spaceId}).populate('contentType', "title").populate('category', 'name').select("name status sys category contentType")
     .exec(function(err, contents){
         var result = {success : false, data : null, error : null };
         if (err)
@@ -84,7 +84,7 @@ var loadContents = function(req, cb)
 
 var findAll = function(req, cb)
 {
-    Contents.find({"sys.spaceId" : req.spaceId}).populate('contentType', "title").populate('category', 'name').exec(function(err, contents){
+    Contents.find({"sys.spaceId" : req.spaceId}).populate('contentType', "title").populate('category', 'name').select("name status sys category contentType").exec(function(err, contents){
         var result = {success : false, data : null, error : null };
         if (err)
         {
