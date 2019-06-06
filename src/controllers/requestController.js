a814060d6f3190dfa1203b69bff5e019e175c0cf
fakhrad <fakhrad@openratio.com>
2019-06-06 12:59:14 +0430

    issue fixed for request thumbnail

--- src/controllers/requestController.js
+++ src/controllers/requestController.js
@@ -313,9 +313,6 @@ var updateContent = function(req, cb)
              if (req.body.contentType)
                 request.contentType = req.body.contentType;
             request.sys.lastUpdateTime = new Date();
-            console.log('---------------------------')
-            console.log(req.body);
-            console.log(request);
             request.save(function(err){
                 if(err)
                 {
