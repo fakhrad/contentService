f273d8cc1ab0e5b42c8abd553873d56727109df2
fakhrad <fakhrad@openratio.com>
2019-08-11 18:20:06 +0430

    content update api issue fixed

--- src/controllers/contentController.js
+++ src/controllers/contentController.js
@@ -266,7 +266,8 @@ var updateContent = function(req, cb) {
       return;
     }
     if (content) {
-      Object.assign(content.fields, req.body.fields);
+      //Object.assign(content.fields, req.body.fields);
+      for (var fld in req.body.fields) content.fields[fld] = fld;
       //content.fields = req.body.fields;
       if (content.status != "draft") {
         var newStatus = {};
