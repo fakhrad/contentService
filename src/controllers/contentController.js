b9bd5a7470983411638393d3f88b6689edc4fd98
fakhrad <fakhrad@openratio.com>
2019-08-11 18:28:27 +0430

    log updated

--- src/controllers/contentController.js
+++ src/controllers/contentController.js
@@ -268,8 +268,9 @@ var updateContent = function(req, cb) {
     if (content) {
       //Object.assign(content.fields, req.body.fields);
       for (var fld in req.body.fields) {
-        console.log(fld);
+        console.log(fld + " : " + req.body.fields[fld]);
         content.fields[fld] = req.body.fields[fld];
+        console.log(fld + " : " + content.fields[fld]);
       }
       //content.fields = req.body.fields;
       if (content.status != "draft") {
